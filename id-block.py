ff4d46f8c60e5c0eaca61f4841ab4c9d6a3d30ed
ignacio samael meyer <139611728+ignack354@users.noreply.github.com>
2024-12-15 21:03:56 -0600

    id-block 1.0.0
    The program looks for the ids of the blocks in Minecraft on the "https://minecraft-ids.grahamedgecombe.com/" page and saves them in a json format

---
+++ id-block.py
@@ -0,0 +1,44 @@
+from playwright.sync_api import sync_playwright
+import json
+import re
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=True)
+    page = browser.new_page()
+    page.goto("https://minecraft-ids.grahamedgecombe.com/")
+    elements = page.query_selector_all(".row")
+    
+    ids = {}
+    
+    with open("results.json", "w") as f:
+        print(len(elements))
+        for element in elements:
+            
+            # Replace line breaks and excess spaces
+            string = element.text_content().replace("\n", " ").strip()
+            
+            print(f"Cleaned string: {string}")  # For debugging
+            
+            # Adjusted regular expression
+            pattern = r'(\d+(:\d+)?)\s+([a-zA-Z0-9_ ]+)\(([^)]+)\)'
+            
+            # Search for matches
+            matches = re.search(pattern, string)
+            if matches:
+                # Find the ID (key)
+                key = matches.group(1)
+                # Find the value (Minecraft ID)
+                value = matches.group(4)
+                value = re.sub("^minecraft:", "", value)
+                # Save to the dictionary
+                ids[key] = value
+            else:
+                print("No match found")
+        
+        # Display the content of the dictionary
+        print(ids)
+        
+        # Save the results in JSON format
+        json.dump(ids, f, indent=4)
+    
+    browser.close()
